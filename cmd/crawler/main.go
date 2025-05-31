441d8aa493e1c108246efe9b3647b60efe0f8ea0
SotaOgawa <ogawa.s.ay@m.titech.ac.jp>
2025-05-31 18:30:00 +0900

    add: cmd/crawler/main.goを実装し実際に稼働を確認

---
+++ cmd/crawler/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"paper-app-backend/internal/conference"
+	"fmt"
+	"strings"
+)
+
+func main(){
+	crawlerICML := conference.NewICMLConferenceCrawler(2023)
+	paperlist, err := crawlerICML.Crawl()
+	if err != nil {
+		fmt.Println("Etrror during crawling: %w", err)
+		return
+	}
+	for _, paper := range paperlist {
+		println("Title:", paper.Title)
+		authors := strings.Join(paper.Authors, ", ")
+		println("Authors:", authors)
+		println("Venue:", paper.Venue)
+		println("Year:", paper.Year)
+	}
+	println("Crawling completed successfully")
+	println("Crawled", len(paperlist), "papers")
+}
