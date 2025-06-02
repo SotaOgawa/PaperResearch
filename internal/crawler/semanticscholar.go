cca4aac6b1ba13f9e2d96debe37dd6d85c1d0536
SotaOgawa <ogawa.s.ay@m.titech.ac.jp>
2025-06-02 12:16:54 +0900

    fix: クエリが正しく登録されるように調整

--- internal/crawler/semanticscholar.go
+++ internal/crawler/semanticscholar.go
@@ -3,11 +3,13 @@ package crawler
 import (
 	"encoding/json"
 	"fmt"
-	"gorm.io/gorm"
 	"io"
 	"net/http"
 	"net/url"
 	"paper-app-backend/internal/model"
+	"strings"
+
+	"gorm.io/gorm"
 	// "strings"
 )
 
@@ -17,7 +19,7 @@ type SemanticScholarConferenceCrawler struct {
 func (c *SemanticScholarConferenceCrawler) Crawl(paper *model.PaperObjectInDB, db *gorm.DB) ([]model.PaperObjectInDB, error) {
 	BASE_URL := "https://api.semanticscholar.org/graph/v1/paper/search"
 
-	req_url := fmt.Sprintf("%s?query=%s&fields=title,year,venue,abstract,referenceCount,citationCount,influentialCitationCount,isOpenAccess,fieldsOfStudy,authors&limit=10", BASE_URL, url.QueryEscape(paper.Title))
+	req_url := fmt.Sprintf(`%s?query=%s&fields=title,year,venue,abstract,citationCount,authors,url,citationStyles&limit=1`, BASE_URL, url.QueryEscape(paper.Title))
 
 	req, err := http.NewRequest("GET", req_url, nil)
 	if err != nil {
@@ -41,20 +43,26 @@ func (c *SemanticScholarConferenceCrawler) Crawl(paper *model.PaperObjectInDB, d
 		Offset int `json:"offset"`
 		Next   int `json:"next"`
 		Data   []struct {
-			PaperID                  string   `json:"paperId"`
-			Title                    string   `json:"title"`
-			Venue                    string   `json:"venue"`
-			Abstract                 string   `json:"abstract"`
-			Year                     int      `json:"year"`
-			ReferenceCount           int      `json:"referenceCount"`
-			CitationCount            int      `json:"citationCount"`
-			InfluentialCitationCount int      `json:"influentialCitationCount"`
-			IsOpenAccess             bool     `json:"isOpenAccess"`
-			FieldsOfStudy            []string `json:"fieldsOfStudy"`
-			Authors                  []struct {
+			PaperID       string `json:"paperId"`
+			URL           string `json:"url"`
+			Title         string `json:"title"`
+			Venue         string `json:"venue"`
+			Year          int    `json:"year"`
+			CitationCount int    `json:"citationCount"`
+			OpenAccessPdf struct {
+				URL        string `json:"url"`
+				Status     any    `json:"status"`
+				License    any    `json:"license"`
+				Disclaimer string `json:"disclaimer"`
+			} `json:"openAccessPdf"`
+			CitationStyles struct {
+				Bibtex string `json:"bibtex"`
+			} `json:"citationStyles"`
+			Authors []struct {
 				AuthorID string `json:"authorId"`
 				Name     string `json:"name"`
 			} `json:"authors"`
+			Abstract string `json:"abstract"`
 		} `json:"data"`
 	}
 
@@ -62,14 +70,34 @@ func (c *SemanticScholarConferenceCrawler) Crawl(paper *model.PaperObjectInDB, d
 		return nil, fmt.Errorf("failed to decode response: %w", err)
 	}
 
+	var papers []model.PaperObjectInDB
+
 	for i, paperData := range response.Data {
 		// 取得した全てのデータを書き出す
-		fmt.Printf("Paper %d: ID: %s, Title: %s, Venue: %s, Year: %d, Abstract: %s, ReferenceCount: %d, CitationCount: %d, InfluentialCitationCount: %d, IsOpenAccess: %t, FieldsOfStudy: %v\n",
-			i, paperData.PaperID, paperData.Title, paperData.Venue, paperData.Year, paperData.Abstract,
-			paperData.ReferenceCount, paperData.CitationCount, paperData.InfluentialCitationCount, paperData.IsOpenAccess, paperData.FieldsOfStudy)
+		fmt.Printf("Paper %d: ID: %s, Title: %s, Venue: %s, Year: %d, Abstract: %s, CitationCount: %d, URL: %s, OpenAccessPDF: %s, Bibtex: %s\n",
+			i+1, paperData.PaperID, paperData.Title, paperData.Venue, paperData.Year,
+			paperData.Abstract, paperData.CitationCount, paperData.URL,
+			paperData.OpenAccessPdf.URL, paperData.CitationStyles.Bibtex)
+		authors := make([]string, len(paperData.Authors))
+		for j, author := range paperData.Authors {
+			authors[j] = author.Name
+		}
+		authors_joined := strings.Join(authors, ", ")
+		paperInDB := model.PaperObjectInDB{
+			Title:         paperData.Title,
+			Conference:    paperData.Venue,
+			Year:          paperData.Year,
+			Authors:       authors_joined,
+			Abstract:      paperData.Abstract,
+			URL:           paperData.URL,
+			CitationCount: paperData.CitationCount,
+			Bibtex:        paperData.CitationStyles.Bibtex,
+			PDFURL:        paperData.OpenAccessPdf.URL,
+		}
+		papers = append(papers, paperInDB)
 	}
 
-	return nil, nil
+	return papers, nil
 }
 
 func NewSemanticScholarConferenceCrawler() *SemanticScholarConferenceCrawler {
